8c1ee8eeda873974aa1c5da66ec9be82737a7bee
Surya Haditama <110581874+Haditamaaa@users.noreply.github.com>
2024-10-01 14:39:26 +0700

    insdel

--- phpdasar8/functions.php
+++ phpdasar8/functions.php
@@ -12,3 +12,44 @@ function query($query)
     }
     return $rows;
 }
+
+function tambah($data)
+{
+    // data elemen tiap form
+    global $conn;
+    $nama = htmlspecialchars($data["nama"]);
+    $nrp = htmlspecialchars($data["nrp"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+
+    // query insert data
+    $query =  "INSERT INTO mahasiswa VALUES (' ', '$nama', '$nrp', '$email', '$jurusan', '$gambar')";
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+function hapus($id)
+{
+    global $conn;
+    mysqli_query($conn, "DELETE FROM mahasiswa where id = $id");
+
+    return mysqli_affected_rows($conn);
+}
+
+
+
+
+
+
+
+
+// // cek data berhasil (+/-)
+// if (mysqli_affected_rows($conn)) {
+//     echo "berhasil";
+// } else {
+//     echo "gagal";
+//     echo "<br>";
+//     echo mysqli_error($conn);
+// }
--- phpdasar8/index.php
+++ phpdasar8/index.php
@@ -10,36 +10,74 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Admin</title>
+    <style>
+        .header a {
+            background-color: #4a90e2;
+            color: white;
+            padding: 10px 15px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            font-size: 16px;
+            margin-left: 30%;
+        }
+
+        a {
+            background-color: #4a90e2;
+            color: white;
+            padding: 10px 15px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            font-size: 16px;
+        }
+
+        .header {
+            margin-bottom: 20px;
+        }
+
+        table {
+            text-align: center;
+        }
+
+        h1 {
+            text-align: center;
+            padding-bottom: 20px;
+        }
+    </style>
 </head>
 
 <body>
-    <h1>DAFTAR MAHASISWA</h1>
-    <img src="/img/example1.jpg" alt="">
+    <div class="header">
+        <h1>DAFTAR MAHASISWA</h1>
+        <a href="tambah.php">Tambah Data Mahasiswa</a>
+    </div>
 
-    <table border="1" cellpadding="10" cellspacing="0">
+
+    <table border="1" cellpadding="10" cellspacing="0" align="center">
         <tr>
             <th>No.</th>
-            <th>Aksi</th>
             <th>Gambar</th>
             <th>NRP</th>
             <th>Nama</th>
             <th>Email</th>
             <th>Jurusan</th>
+            <th>Aksi</th>
         </tr>
 
         <?php $i = 1; ?>
         <?php foreach ($mahasiswa as $row) : ?>
             <tr>
                 <td><?php echo $i; ?></td>
-                <td>
-                    <a href="#">ubah</a>
-                    <a href="#">hapus</a>
-                </td>
                 <td><img src="img/<?php echo $row["gambar"]; ?>" width="50" alt=""></td>
                 <td><?php echo $row["nrp"] ?></td>
                 <td><?php echo $row["nama"] ?></td>
                 <td><?php echo $row["email"] ?></td>
                 <td><?php echo $row["jurusan"] ?></td>
+                <td>
+                    <a href="#">ubah</a>
+                    <a href="hapus.php?id=<?php echo $row["id"]; ?>" onclick="return confirm('yakin?');">hapus</a>
+                </td>
             </tr>
             <?php $i++; ?>
         <?php endforeach; ?>
